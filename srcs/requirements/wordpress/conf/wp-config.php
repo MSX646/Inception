5599d26476f5b8cdb308d4770f4e3d2fe38a08ff
MSX646 <beowulf5222@gmail.com>
2022-10-22 17:28:09 -0400

    working fine

---
+++ srcs/requirements/wordpress/conf/wp-config.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * The base configuration for WordPress
+ *
+ * The wp-config.php creation script uses this file during the installation.
+ * You don't have to use the web site, you can copy this file to "wp-config.php"
+ * and fill in the values.
+ *
+ * This file contains the following configurations:
+ *
+ * * Database settings
+ * * Secret keys
+ * * Database table prefix
+ * * ABSPATH
+ *
+ * @link https://wordpress.org/support/article/editing-wp-config-php/
+ *
+ * @package WordPress
+ */
+define('DB_NAME', getenv('DB_NAME'));
+define('DB_USER', getenv('DB_USER'));
+define('DB_PASSWORD', getenv('DB_PASS'));
+define( 'DB_HOST', 'mariadb' );
+define( 'DB_CHARSET', 'utf8' );
+define( 'DB_COLLATE', '' );
+define('FS_METHOD','direct');
+$table_prefix = 'wp_';
+define( 'WP_DEBUG', true );
+#if ( ! defined( 'ABSPATH' ) ) {
+#define( 'ABSPATH', __DIR__ . '/' );}
+#define( 'WP_REDIS_HOST', 'redis' );
+#define( 'WP_REDIS_PORT', 6379 );
+#define( 'WP_REDIS_TIMEOUT', 1 );
+#define( 'WP_REDIS_READ_TIMEOUT', 1 );
+#define( 'WP_REDIS_DATABASE', 0 );
+require_once ABSPATH . 'wp-settings.php';
